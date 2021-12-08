7e36cd556ef3af6e1c9ddc99f0805940a45cd5f8
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-12-08 11:34:14 -0800

    form event listener works

--- app.js
+++ app.js
@@ -1,41 +1,117 @@
 // import functions and grab DOM elements
+import { renderPoll } from './render-utils.js';
+
 const form = document.querySelector('form');
 
 const questionEl = document.getElementById('create-question');
-const question1Title = document.getElementById('current-option-1');
-const question1Votes = document.getElementById('current-option-1-votes');
-const question2Title = document.getElementById('current-option-2');
-const question2Votes = document.getElementById('current-option-2-votes');
+const option1TitleEl = document.getElementById('current-option-1');
+const option1VotesEl = document.getElementById('current-option-1-votes');
+const option2TitleEl = document.getElementById('current-option-2');
+const option2VotesEl = document.getElementById('current-option-2-votes');
 
 const createPollButton = document.getElementById('create-poll');
-const Vote1Button = document.getElementById('vote-1');
-const Vote2Button = document.getElementById('vote-2');
-const Undo1Button = document.getElementById('undo-1');
-const Undo2Button = document.getElementById('undo-2');
+const vote1Button = document.getElementById('vote-1');
+const vote2Button = document.getElementById('vote-2');
+const undo1Button = document.getElementById('undo-1');
+const undo2Button = document.getElementById('undo-2');
 const closePollButton = document.getElementById('close-poll-button');
 
 const pastPollsEl = document.getElementById('past-polls');
 
 
-console.log(
+/* console.log(
     form,
     questionEl,
-    question1Title,
-    question1Votes,
-    question2Title,
-    question2Votes,
+    question1TitleEl,
+    question1VotesEl,
+    question2TitleEl,
+    question2VotesEl,
     createPollButton,
-    Vote1Button,
-    Vote2Button,
-    Undo1Button,
-    Undo2Button,
+    vote1Button,
+    vote2Button,
+    undo1Button,
+    undo2Button,
     closePollButton,
     pastPollsEl
-);
+); */
 
 // let state
+let question = '';
+let option1Title = '';
+let option2Title = '';
+let option1Votes = 0;
+let option2Votes = 0;
 
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+  
+    question = data.get('create-question');
+    option1Title = data.get('create-option-1');
+    option2Title = data.get('create-option-2');
+
+
+    //const poll = makePoll();
+    //renderPoll(poll);
+    displayCurrentPoll();
+});
+
+vote1Button.addEventListener('click', () => {
+    option1Votes++;
+
+    option1VotesEl.textContent = option1Votes;
+});
+
+vote2Button.addEventListener('click', () => {
+    option2Votes++;
+
+    option2VotesEl.textContent = option2Votes;
+});
+
+undo1Button.addEventListener('click', () => {
+    option1Votes--;
+
+    option1VotesEl.textContent = option1Votes;
+});
+
+undo2Button.addEventListener('click', () => {
+    option2Votes--;
+  
+    option2VotesEl.textContent = option2Votes;
+});
+
+closePollButton.addEventListener('click', () => {
+
+});
+
+
+function makePoll() {
+  
+    return {
+        question: question,
+        option1Title: option1Title,
+        option2Title: option2Title,
+        option1Votes: option1Votes,
+        option2Votes: option2Votes
+    };
+  
+}
+
+function displayCurrentPoll() {
+    questionEl.textContent = question;
+    option1TitleEl.textContent = option1Title;
+    option2TitleEl.textContent = option2Title;
+    option1VotesEl.textContnet = option1Votes;
+    option2VotesEl.textConent = option2Votes;
+}
+
+//displayAllPolls() {
+
+//}
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ render-utils.js
@@ -0,0 +1,24 @@
+export function renderPoll(poll) {
+    const pollContainer = document.createElement('div');
+    const pollQuestionSpot = document.createElement('div');
+    const option1Spot = document.createElement('p');
+    const option2Spot = document.createElement('p');
+    const option1VotesSpot = document.createElement('p');
+    const option2VotesSpot = document.createElement('p');
+
+    pollContainer.classList.add('poll');
+    pollQuestionSpot.textContent = poll.question;
+    option1Spot.textContent = poll.option1Title;
+    option2Spot.textContent = poll.option2Title;
+    option1VotesSpot.textConent = poll.option1Votes;
+    option2VotesSpot.textContent = poll.option2Votes;
+
+    pollContainer.append(pollQuestionSpot, option1Spot, option2Spot, option1VotesSpot, option2VotesSpot);
+
+    return pollContainer;
+}
+
+
+export function renderOption(name, score) {
+
+}
